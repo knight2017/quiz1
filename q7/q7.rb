d2ab00c044f29ca0d402750a131827997c398a9a
Wei Yi <weiyi2017@gmail.com>
2016-05-30 10:34:21 -0700

    Questions 7

---
+++ q7/q7.rb
@@ -0,0 +1,21 @@
+# module HelperMethods
+
+  def titlize(a)
+      words = a.split
+      words.map! do |word|
+## the commented out line below will capitalize the first word even if they are the "in", "of", "and" and etc.        
+        # if words.index(word) != 0 && (word == "of" || word == "and" ||  word == "the" ||word == "in" || word == "from" ||word == "or")
+        if (word == "of" || word == "and" ||  word == "the" ||word == "in" || word == "from" ||word == "or")
+
+          word
+        else
+          word.capitalize
+        end
+      end
+
+     words.join(" ")
+
+  end
+# end
+
+p titlize("in the summer of love or the from the heaven and hell in")
